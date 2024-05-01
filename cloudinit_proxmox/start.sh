3b3fc7453d8f0ba1b2e86a801037bf0ced08a9ae
Your Name <you@example.com>
2024-05-01 16:59:33 +0300

    test is working

--- cloudinit_proxmox/start.sh
+++ cloudinit_proxmox/start.sh
@@ -3,36 +3,28 @@
 # Set dialog menu title
 TITLE="Select OS"
 
-# Initialize os_choice
-os_choice=""
-
 # Prompt user to choose OS
 items=(1 "Debian 12"
        2 "Ubuntu 22.04"
-       3 "Custom OS")
+       3 "Custom ISO")
 
 while choice=$(dialog --title "$TITLE" \
                  --menu "Please select" 10 40 3 "${items[@]}" \
                  2>&1 >/dev/tty)
 do
-  case $choice in
-     1) os_choice="Debian 12"; selected_os="debian-12-generic-amd64.qcow2";;
-     2) os_choice="Ubuntu 22.04"; selected_os="ubuntu-22.04-server-cloudimg-amd64.img";;
-     3) read -p "Enter custom ISO URL (leave empty to proceed without custom ISO): " custom_iso_url
-       if [[ -n "$custom_iso_url" ]]; then
-         wget "$custom_iso_url" -O custom.iso
-         selected_os="custom.iso"
-       else
-         echo "ISO already exists. Proceeding."
-       fi
-       ;;
+    case $choice in
+        1) os_choice="Debian 12";;
+        2) os_choice="Ubuntu 22.04";;
+        3) read -p "Enter custom ISO URL (leave empty to proceed without custom ISO): " custom_iso_url
+           if [[ -n "$custom_iso_url" ]]; then
+               wget "$custom_iso_url" -O custom.iso
+               selected_os="custom.iso"
+           else
+               echo "ISO already exists. Proceeding."
+           fi
+           ;;
     esac
-  done
-clear # Clear after user pressed Cancel
-
-
-# Set a variable to track the selected OS
-selected_os=""
+done
 
 # Check if the ISO file exists or allow custom ISO
 if [[ "$os_choice" == "Debian 12" && ! -f "debian-12-generic-amd64.qcow2" ]]; then
@@ -42,7 +34,7 @@ elif [[ "$os_choice" == "Ubuntu 22.04" && ! -f "ubuntu-22.04-server-cloudimg-amd
     wget https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img -O ubuntu-22.04-server-cloudimg-amd64.img
     selected_os="ubuntu-22.04-server-cloudimg-amd64.img"
 else
-    read -p "Enter custom ISO URL (leave empty to proceed without custom ISO): " custom_iso_url
+      echo "Enter custom ISO URL (leave empty to proceed without custom ISO): " custom_iso_url
     if [[ -n "$custom_iso_url" ]]; then
         wget "$custom_iso_url" -O custom.iso
         selected_os="custom.iso"
@@ -51,8 +43,11 @@ else
     fi
 fi
 
+# Set a variable to track the selected OS
+selected_os=""
+
 # Prompt user if they want to add a guest agent
-read -p "Do you want to add a guest agent? (y/n): " add_guest_agent
+echo "Do you want to add a guest agent? (y/n): " add_guest_agent
 
 # Install the guest agent if requested
 if [[ "$add_guest_agent" == "y" ]]; then
@@ -70,9 +65,6 @@ read -p "Enter network bridge (vmbr0):" network_bridge
 read -p "Enter disk size (GB):" disk_size
 read -p "Enter storage pool (local-lvm):" storage_pool
 
-
-
-
 # Create cloud-init template
 qm create $vm_number --name $name --memory $memory --net0 virtio,bridge=$network_bridge
 qm importdisk $vm_number $selected_os $storage_pool
@@ -81,7 +73,6 @@ qm set $vm_number --ide2 $storage_pool:cloudinit
 qm set $vm_number --boot c --bootdisk scsi0
 qm set $vm_number --ipconfig0 ip=dhcp
 qm resize $vm_number scsi0 $disk_size 
-
 
 # Prompt user if they want to create a template
 read -p "Do you want to create a template? (y/n): " create_template
