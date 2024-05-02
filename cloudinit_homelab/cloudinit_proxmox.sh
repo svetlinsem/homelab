abc5539fe8ea9d9e920ec70991ba3302d9ab0e5b
Your Name <you@example.com>
2024-05-02 21:50:13 +0300

    new version

--- cloudinit_homelab/cloudinit_proxmox.sh
+++ cloudinit_homelab/cloudinit_proxmox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set dialog menu title
-whiptail --title "Homelab Toolbox" --msgbox "Homelab Tool is easy way to create cloudinit VM in Proxmox" 8 78
+whiptail --title "Homelab Toolbox" --msgbox "Homelab Tool is an easy way to create cloudinit VMs in Proxmox" 8 78
 
 # Initialize variables
 os_choice=""
@@ -24,11 +24,12 @@ choice=$(whiptail --title "Check list example" --radiolist \
 3>&1 1>&2 2>&3)
 
 case $choice in
-    "Debian") os_choice="Debian 12";;
-    "Ubuntu") os_choice="Ubuntu 22.04";;
-    "Custom OS") custom_iso_url=$(whiptail --inputbox "Enter Custom ISO URL:" 8 39 --title "Custom OS URL" 3>&1 1>&2 2>&3)
+    "Debian") os_choice="debian";;
+    "Ubuntu") os_choice="ubuntu";;
+    "Custom OS") 
+        custom_iso_url=$(whiptail --inputbox "Enter Custom ISO URL:" 8 39 --title "Custom OS URL" 3>&1 1>&2 2>&3)
         if [[ -n "$custom_iso_url" ]]; then
-            wget "$custom_iso_url" -O custom.iso
+            wget "$custom_iso_url" -O custom.iso || { echo "Error downloading custom ISO"; exit 1; }
             selected_os="custom.iso"
         else
             echo "ISO already exists. Proceeding."
@@ -39,20 +40,12 @@ esac
 clear # Clear after user pressed Cancel
 
 # Check if the ISO file exists or allow custom ISO
-if [[ "$os_choice" == "Debian 12" && ! -f "debian-12-generic-amd64.qcow2" ]]; then
-    wget https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2 -O debian-12-generic-amd64.qcow2
+if [[ "$os_choice" == "debian" && ! -f "debian-12-generic-amd64.qcow2" ]]; then
+    wget https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2 -O debian-12-generic-amd64.qcow2 || { echo "Error downloading Debian ISO"; exit 1; }
     selected_os="debian-12-generic-amd64.qcow2"
-elif [[ "$os_choice" == "Ubuntu 22.04" && ! -f "ubuntu-22.04-server-cloudimg-amd64.img" ]]; then
-    wget https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img -O ubuntu-22.04-server-cloudimg-amd64.img
+elif [[ "$os_choice" == "ubuntu" && ! -f "ubuntu-22.04-server-cloudimg-amd64.img" ]]; then
+    wget https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img -O ubuntu-22.04-server-cloudimg-amd64.img || { echo "Error downloading Ubuntu ISO"; exit 1; }
     selected_os="ubuntu-22.04-server-cloudimg-amd64.img"
-else
-      echo "Enter custom ISO URL (leave empty to proceed without custom ISO): " custom_iso_url
-    if [[ -n "$custom_iso_url" ]]; then
-        wget "$custom_iso_url" -O custom.iso
-        selected_os="custom.iso"
-    else
-        echo "ISO already exists. Proceeding."
-    fi
 fi
 
 # Prompt user if they want to add a guest agent
@@ -62,8 +55,8 @@ add_guest_agent=$(whiptail --yesno "Do you want to add a guest agent?" 8 78 --ti
 if [[ "$add_guest_agent" == "true" ]]; then
     # Add your installation command here
     echo "Installing guest agent..."
-    apt install -y libguestfs-tools
-    virt-customize --install qemu-guest-agent -a $selected_os
+    apt install -y libguestfs-tools || { echo "Error installing guest agent"; exit 1; }
+    virt-customize --install qemu-guest-agent -a "$selected_os" || { echo "Error installing guest agent"; exit 1; }
 fi
 
 # Prompt user for VM configuration
@@ -74,15 +67,14 @@ memory=$(whiptail --inputbox "Enter memory (MB):" 8 78 --title "VM Configuration
 network_bridge=$(whiptail --inputbox "Enter network bridge (vmbr0):" 8 78 --title "VM Configuration" 3>&1 1>&2 2>&3)
 disk_size=$(whiptail --inputbox "Enter disk size (GB):" 8 78 --title "VM Configuration" 3>&1 1>&2 2>&3)
 
-
 # Create cloud-init template
-qm create $vm_number --name $name --memory $memory --net0 virtio,bridge=$network_bridge
-qm importdisk $vm_number $selected_os $storage_pool
-qm set $vm_number --scsihw virtio-scsi-pci --scsi0 $storage_pool:vm-$vm_number-disk-0
-qm set $vm_number --ide2 $storage_pool:cloudinit
-qm set $vm_number --boot c --bootdisk scsi0
-qm set $vm_number --ipconfig0 ip=dhcp
-qm resize $vm_number scsi0 $disk_size
+qm create "$vm_number" --name "$name" --memory "$memory" --net0 virtio,bridge="$network_bridge" || { echo "Error creating VM"; exit 1; }
+qm importdisk "$vm_number" "$selected_os" "$storage_pool" || { echo "Error importing disk"; exit 1; }
+qm set "$vm_number" --scsihw virtio-scsi-pci --scsi0 "$storage_pool":vm-"$vm_number"-disk-0 || { echo "Error setting SCSI"; exit 1; }
+qm set "$vm_number" --ide2 "$storage_pool":cloudinit || { echo "Error setting IDE"; exit 1; }
+qm set "$vm_number" --boot c --bootdisk scsi0 || { echo "Error setting boot disk"; exit 1; }
+qm set "$vm_number" --ipconfig0 ip=dhcp
+
 
 # Prompt user if they want to create a template
 read -p "Do you want to create a template? (y/n): " create_template
