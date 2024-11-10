379a4190f8feabeb70b5b05ef8c6a4ad5c1b3fb6
svetlinsem <johndoe@example.com>
2024-11-10 20:41:59 +0200

    update invetory

--- terraform/vm/main.tf
+++ terraform/vm/main.tf
@@ -1,15 +1,15 @@
 resource "proxmox_vm_qemu" "k8smastertest" {
   target_node       = var.target_node
-  name              = "dockertraining"
-  vmid              = 900
+  name              = "coolify"
+  vmid              = 220
   count             = 1 
   clone             = "debian12"
   os_type           = "cloud-init"
   scsihw            = "virtio-scsi-pci"
   boot              = "order=scsi0;ide2"
   cpu               = "host"
-  cores             = 2
-  memory            = 4000
+  cores             = 4
+  memory            = 8000
   vm_state          = "running"
   agent             = 1
   onboot            = true
@@ -33,6 +33,6 @@ resource "proxmox_vm_qemu" "k8smastertest" {
   # Cloud Init Settings
   # Reference: https://pve.proxmox.com/wiki/Cloud-Init_Support
   cloudinit_cdrom_storage = var.storage_backend
-  ipconfig0 = "ip=192.168.1.90/24,gw=192.168.1.1"
+  ipconfig0 = "ip=192.168.1.22/24,gw=192.168.1.1"
   nameserver = var.nameservers
 } 
--- terraform/vm/vars.tf
+++ terraform/vm/vars.tf
@@ -1,4 +1,4 @@
 variable "target_node" { default = "proxmoxprod" }
 variable "storage_backend" { default = "apps" }
 variable "cidr" { default = "24" }
-variable "nameservers" { default = "8.8.8.8 1.1.1.1" }
+variable "nameservers" { default = "" }
